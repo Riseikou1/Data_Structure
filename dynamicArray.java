59ae37fbaa6ead04fa1701dd991cd7e5c89bc03b
Tmk <temka4705@gmail.com>
2025-03-15 14:25:16 +0900

    Update dynamicArray.java

--- dynamicArray.java
+++ dynamicArray.java
@@ -1,107 +1,26 @@
-package Structure;
-public class Dynamic_Arrays {
-    int size;
-    int capacity=10;
-    Object[] array;
-    public Dynamic_Arrays(){
-        this.array = new Object[capacity];
+package Search;
+// interpolation search  =  improvement over binary search best used for "uniformly" distributed data
+//                          "guesses" where a value might be based on calculated probe results
+//                          if probe is incorrect,search area is narrowed,and a new probe is calculated.
+//                      average case : O(log(log(n)))
+//                      worst case :   O(n) [values increase exponentially]
+public class Interpolation_Search {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        int index = interpolationSearch(array,8);    
+        if(index !=-1 )    System.out.println("Element found at: "+index);
+        else     System.out.println("Element not found.");
     }
-    public Dynamic_Arrays(int capacity) {
-        this.capacity = capacity;
-        this.array = new Object[capacity];
-    }
-    public void add(Object data) {
-        if(size >= capacity) {
-            grow();
-        }
-        array[size] = data;
-        size ++ ;
-    }
-    public void insert(int index, Object data){
-        if(size >= capacity) {
-            grow();
-        }
-        for(int i=size;i>index;i--){
-            array[i] = array[i-1];
-        }
-        array[index] = data;
-        size ++ ;
-    }
-    public void delete(Object data){
-        for(int i=0;i<size;i++){
-            if(array[i] == data) {
-                for(int j=0;j<(size-i-1);j++){
-                    array[i+j] = array[i+j+1];
-                }
-                array[size-1] = null;
-                size -- ;
-                if(size <= (int)capacity/2){
-                    shrink();
-                }
-                break;
-            }
-        }
-    }
-    public int search(Object data){
-        for(int i=0;i<size;i++){
-            if(array[i] == data){
-                return i;
-            } 
+    private static int interpolationSearch(int[] array,int value) {
+        int high = array.length-1;
+        int low = 0;
+        while(value>=array[low] && value<=array[high] && low<=high){
+            int probe = (high-low)*(value-array[low])/(array[high]-array[low]);
+            System.out.println("probe: "+ probe);
+            if(array[probe]==value)      return probe;  // zaaval curly bracket hiihgu,ingeed return hiichij bas boldog.
+            else if(array[probe]<value)  low = probe + 1;
+            else   high = probe -1;
         }
         return -1;
-    }   
-    public void grow(){
-        int newCapacity = capacity*2;
-        Object[] newArray = new Object[newCapacity];
-        for(int i=0;i<size;i++){
-            newArray[i] = array[i];
-        }
-        capacity = newCapacity;
-        array = newArray;
-    }
-    public void shrink(){
-        int newCapacity = capacity/2;
-        Object[] newArray = new Object[newCapacity];
-        for(int i=0;i<size;i++){
-            newArray[i] = array[i];
-        }
-        capacity = newCapacity;
-        array = newArray;
-    }
-    public boolean isEmpty(){
-        return size == 0;
-    }
-    public String toString() {
-        String string = "";
-        for(int i=0;i<capacity;i++){
-            string += array[i] + ", ";
-        }
-        if(string !=""){
-            string = "["+ string.substring(0,string.length()-2)+"]"; // -2 gej suuliin elementnii ardah baigaa taslal, bolon space-iig zailuulj baigaa.
-        }
-        else {
-            string = "[]";
-        }
-        return string ;
-    }
-    public static void main(String[] args) {
-        Dynamic_Arrays dynamicArray = new Dynamic_Arrays(6);
-        dynamicArray.add("A");
-        dynamicArray.add("B");
-        dynamicArray.add("C");
-        dynamicArray.add("D");
-        dynamicArray.add("E");
-        dynamicArray.add("F");
-        dynamicArray.insert(0,"Temuujin");
-        System.out.println("capacity: "+dynamicArray.capacity);
-        dynamicArray.delete("A");
-        dynamicArray.delete("B");
-        dynamicArray.delete("C");
-        dynamicArray.delete("D");
-        dynamicArray.delete("E");
-        System.out.println("index of your shit: "+ dynamicArray.search("C"));
-        System.out.println("size: "+dynamicArray.size);
-        System.out.println("capacity: "+dynamicArray.capacity);
-        System.out.println(dynamicArray); // zaaval toString gej duudah heregguigeer bas bolno.
     }
 }
