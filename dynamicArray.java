9fc6113b53be41264b55fa1fb8193390d4f74fd8
Tmk <temka4705@gmail.com>
2025-03-17 23:29:59 +0900

    Update dynamicArray.java

--- dynamicArray.java
+++ dynamicArray.java
@@ -1,127 +1,87 @@
-#include <iostream>
-using namespace std;
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
-    
-    Node(int data) {
-        this->data = data;
-        left = right = nullptr;
-    }
-};
+class BinaryTree:
+    def __init__(self):
+        self.root = None
 
-class BinaryTree {
-private:
-    Node* root;
+    def insert(self, data):
+        self.root = self._insert_helper(self.root, data)
 
-    Node* insertHelper(Node* root, Node* node) {
-        if (root == nullptr) {
-            return node;
-        }
-        if (node->data < root->data) {
-            root->left = insertHelper(root->left, node);
-        } else {
-            root->right = insertHelper(root->right, node);
-        }
-        return root;
-    }
+    def _insert_helper(self, root, data):
+        if root is None:
+            return Node(data)
+        if data < root.data:
+            root.left = self._insert_helper(root.left, data)
+        else:
+            root.right = self._insert_helper(root.right, data)
+        return root
 
-    void displayHelper(Node* root) {
-        if (root != nullptr) {
-            displayHelper(root->left);
-            cout << root->data << " ";
-            displayHelper(root->right);
-        }
-    }
+    def display(self):
+        self._display_helper(self.root)
+        print()
 
-    bool searchHelper(Node* root, int data) {
-        if (root == nullptr) return false;
-        if (root->data == data) return true;
-        if (data < root->data) return searchHelper(root->left, data);
-        return searchHelper(root->right, data);
-    }
+    def _display_helper(self, root):
+        if root:
+            self._display_helper(root.left)
+            print(root.data, end=" ")
+            self._display_helper(root.right)
 
-    Node* removeHelper(Node* root, int data) {
-        if (root == nullptr) {
-            return root;
-        } else if (data < root->data) {
-            root->left = removeHelper(root->left, data);
-        } else if (data > root->data) {
-            root->right = removeHelper(root->right, data);
-        } else {
-            if (root->left == nullptr && root->right == nullptr) {
-                delete root;
-                return nullptr;
-            } else if (root->right != nullptr) {
-                root->data = successor(root);
-                root->right = removeHelper(root->right, root->data);
-            } else {
-                root->data = predecessor(root);
-                root->left = removeHelper(root->left, root->data);
-            }
-        }
-        return root;
-    }
+    def search(self, data):
+        return self._search_helper(self.root, data)
 
-    int successor(Node* root) {
-        root = root->right;
-        while (root->left != nullptr) {
-            root = root->left;
-        }
-        return root->data;
-    }
+    def _search_helper(self, root, data):
+        if root is None:
+            return False
+        if root.data == data:
+            return True
+        return self._search_helper(root.left, data) if data < root.data else self._search_helper(root.right, data)
 
-    int predecessor(Node* root) {
-        root = root->left;
-        while (root->right != nullptr) {
-            root = root->right;
-        }
-        return root->data;
-    }
+    def remove(self, data):
+        if self.search(data):
+            self.root = self._remove_helper(self.root, data)
+        else:
+            print("Couldn't find the data.")
 
-public:
-    BinaryTree() { root = nullptr; }
+    def _remove_helper(self, root, data):
+        if root is None:
+            return root
+        if data < root.data:
+            root.left = self._remove_helper(root.left, data)
+        elif data > root.data:
+            root.right = self._remove_helper(root.right, data)
+        else:
+            if root.left is None and root.right is None:
+                return None
+            elif root.right is not None:
+                root.data = self._successor(root)
+                root.right = self._remove_helper(root.right, root.data)
+            else:
+                root.data = self._predecessor(root)
+                root.left = self._remove_helper(root.left, root.data)
+        return root
 
-    void insertNode(int data) {
-        root = insertHelper(root, new Node(data));
-    }
+    def _successor(self, root):
+        root = root.right
+        while root.left is not None:
+            root = root.left
+        return root.data
 
-    void display() {
-        displayHelper(root);
-        cout << endl;
-    }
+    def _predecessor(self, root):
+        root = root.left
+        while root.right is not None:
+            root = root.right
+        return root.data
 
-    bool search(int data) {
-        return searchHelper(root, data);
-    }
 
-    void remove(int data) {
-        if (search(data)) {
-            root = removeHelper(root, data);
-        } else {
-            cout << "Couldn't find the data." << endl;
-        }
-    }
-};
+if __name__ == "__main__":
+    tree = BinaryTree()
+    for value in [5, 1, 9, 2, 7, 3, 6, 4, 8]:
+        tree.insert(value)
 
-int main() {
-    BinaryTree tree;
-    tree.insertNode(5);
-    tree.insertNode(1);
-    tree.insertNode(9);
-    tree.insertNode(2);
-    tree.insertNode(7);
-    tree.insertNode(3);
-    tree.insertNode(6);
-    tree.insertNode(4);
-    tree.insertNode(8);
-
-    tree.remove(4);
-    tree.display();
-    cout << tree.search(4) << endl;
-
-    return 0;
-}
+    tree.remove(4)
+    tree.display()
+    print(tree.search(4))
