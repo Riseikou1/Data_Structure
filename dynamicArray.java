580c12ccb98c11f7a241b35a41678f0c21f2b960
Tmk <temka4705@gmail.com>
2025-03-17 22:58:13 +0900

    Update dynamicArray.java

--- dynamicArray.java
+++ dynamicArray.java
@@ -1,91 +1,127 @@
 #include <iostream>
-#include <vector>
-#include <list>
-#include <string>
-#include <algorithm>
+using namespace std;
 
-class SimpleHashMap {
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int data) {
+        this->data = data;
+        left = right = nullptr;
+    }
+};
+
+class BinaryTree {
 private:
-    int size;
-    std::vector<std::list<std::pair<std::string, std::string>>> buckets;
+    Node* root;
 
-    int hashFunction(const std::string& key) {
-        int numericSum = 0;
-        for (char ch : key) {
-            if (isdigit(ch)) {
-                numericSum += ch - '0';
+    Node* insertHelper(Node* root, Node* node) {
+        if (root == nullptr) {
+            return node;
+        }
+        if (node->data < root->data) {
+            root->left = insertHelper(root->left, node);
+        } else {
+            root->right = insertHelper(root->right, node);
+        }
+        return root;
+    }
+
+    void displayHelper(Node* root) {
+        if (root != nullptr) {
+            displayHelper(root->left);
+            cout << root->data << " ";
+            displayHelper(root->right);
+        }
+    }
+
+    bool searchHelper(Node* root, int data) {
+        if (root == nullptr) return false;
+        if (root->data == data) return true;
+        if (data < root->data) return searchHelper(root->left, data);
+        return searchHelper(root->right, data);
+    }
+
+    Node* removeHelper(Node* root, int data) {
+        if (root == nullptr) {
+            return root;
+        } else if (data < root->data) {
+            root->left = removeHelper(root->left, data);
+        } else if (data > root->data) {
+            root->right = removeHelper(root->right, data);
+        } else {
+            if (root->left == nullptr && root->right == nullptr) {
+                delete root;
+                return nullptr;
+            } else if (root->right != nullptr) {
+                root->data = successor(root);
+                root->right = removeHelper(root->right, root->data);
+            } else {
+                root->data = predecessor(root);
+                root->left = removeHelper(root->left, root->data);
             }
         }
-        return numericSum % size;
+        return root;
+    }
+
+    int successor(Node* root) {
+        root = root->right;
+        while (root->left != nullptr) {
+            root = root->left;
+        }
+        return root->data;
+    }
+
+    int predecessor(Node* root) {
+        root = root->left;
+        while (root->right != nullptr) {
+            root = root->right;
+        }
+        return root->data;
     }
 
 public:
-    SimpleHashMap(int size = 10) : size(size), buckets(size) {}
+    BinaryTree() { root = nullptr; }
 
-    void put(const std::string& key, const std::string& value) {
-        int index = hashFunction(key);
-        auto& bucket = buckets[index];
-
-        for (auto& pair : bucket) {
-            if (pair.first == key) {
-                pair.second = value; // Update existing key
-                return;
-            }
-        }
-        bucket.emplace_back(key, value); // Add new key-value pair
+    void insertNode(int data) {
+        root = insertHelper(root, new Node(data));
     }
 
-    std::string get(const std::string& key) {
-        int index = hashFunction(key);
-        auto& bucket = buckets[index];
-
-        for (const auto& pair : bucket) {
-            if (pair.first == key) {
-                return pair.second;
-            }
-        }
-        return "Key not found";
+    void display() {
+        displayHelper(root);
+        cout << endl;
     }
 
-    void remove(const std::string& key) {
-        int index = hashFunction(key);
-        auto& bucket = buckets[index];
-
-        bucket.remove_if([&](const std::pair<std::string, std::string>& pair) {
-            return pair.first == key;
-        });
+    bool search(int data) {
+        return searchHelper(root, data);
     }
 
-    void printMap() {
-        std::cout << "Hash Map Contents:" << std::endl;
-        for (size_t i = 0; i < buckets.size(); i++) {
-            std::cout << "Bucket " << i << " : ";
-            for (const auto& pair : buckets[i]) {
-                std::cout << "(" << pair.first << ", " << pair.second << ") ";
-            }
-            std::cout << std::endl;
+    void remove(int data) {
+        if (search(data)) {
+            root = removeHelper(root, data);
+        } else {
+            cout << "Couldn't find the data." << endl;
         }
     }
 };
 
 int main() {
-    SimpleHashMap hashMap(10);
+    BinaryTree tree;
+    tree.insertNode(5);
+    tree.insertNode(1);
+    tree.insertNode(9);
+    tree.insertNode(2);
+    tree.insertNode(7);
+    tree.insertNode(3);
+    tree.insertNode(6);
+    tree.insertNode(4);
+    tree.insertNode(8);
 
-    hashMap.put("123-4567", "Charlotte");
-    hashMap.put("123-4568", "Thomas");
-    hashMap.put("123-4569", "Jens");
-    hashMap.put("123-4570", "Peter");
-    hashMap.put("123-4571", "Lisa");
-    hashMap.put("123-4572", "Adele");
-    hashMap.put("123-4573", "Michaela");
-    hashMap.put("123-4574", "Bob");
-
-    hashMap.printMap();
-
-    std::cout << "\nName associated with '123-4570': " << hashMap.get("123-4570") << std::endl;
-    std::cout << "Updating the name for '123-4570' to 'Temuujin'\n";
-    hashMap.put("123-4570", "Temuujin");
-    std::cout << "Name associated with '123-4570': " << hashMap.get("123-4570") << std::endl;
+    tree.remove(4);
+    tree.display();
+    cout << tree.search(4) << endl;
 
     return 0;
 }
