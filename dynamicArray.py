ffb89d2882f804e48bc75c0715e1d0e25743567f
Tmk <temka4705@gmail.com>
2025-03-19 23:59:50 +0900

    Update dynamicArray.py

--- dynamicArray.py
+++ dynamicArray.py
@@ -1,61 +1,53 @@
 class Graph:
     def __init__(self, size):
-        self.adj_matrix = [[0] * size for _ in range(size)]
         self.size = size
-        self.vertex_data = [''] * size
-        self.parent = [i for i in range(size)]  # Union-Find array
+        self.adj_list = [[] for _ in range(size)]  # Use adjacency list instead of matrix
+        self.parent = list(range(size))  # Union-Find parent array
+        self.rank = [0] * size  # Rank array for union by rank
 
     def add_edge(self, u, v):
         if 0 <= u < self.size and 0 <= v < self.size:
-            self.adj_matrix[u][v] = 1
-            self.adj_matrix[v][u] = 1
-
-    def add_vertex_data(self, vertex, data):
-        if 0 <= vertex < self.size:
-            self.vertex_data[vertex] = data
+            self.adj_list[u].append(v)
+            self.adj_list[v].append(u)
 
     def find(self, i):
-        if self.parent[i] == i:
-            return i
-        return self.find(self.parent[i])
+        """Find root of i with path compression."""
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])  # Path compression
+        return self.parent[i]
 
     def union(self, x, y):
-        x_root = self.find(x)
-        y_root = self.find(y)
-        print('Union:',self.vertex_data[x],'+',self.vertex_data[y])
-        self.parent[x_root] = y_root
-        print(self.parent,'\n')
+        """Union by rank: attach smaller tree under larger tree."""
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1  # Increase rank of new root
 
     def is_cyclic(self):
-        for i in range(self.size):
-            for j in range(i + 1, self.size):
-                if self.adj_matrix[i][j]:
-                    x = self.find(i)
-                    y = self.find(j)
-                    if x == y:
-                        return True
-                    self.union(x, y)
+        """Detect cycle using Union-Find (Disjoint Set)."""
+        for u in range(self.size):
+            for v in self.adj_list[u]:
+                if u < v:  # To avoid duplicate checks
+                    root_u = self.find(u)
+                    root_v = self.find(v)
+                    if root_u == root_v:
+                        return True  # Cycle detected
+                    self.union(root_u, root_v)
         return False
 
+
+# Example Usage
 g = Graph(7)
 
-g.add_vertex_data(0, 'A')
-g.add_vertex_data(1, 'B')
-g.add_vertex_data(2, 'C')
-g.add_vertex_data(3, 'D')
-g.add_vertex_data(4, 'E')
-g.add_vertex_data(5, 'F')
-g.add_vertex_data(6, 'G')
-
-g.add_edge(1, 0)  # B - A
-g.add_edge(0, 3)  # A - D
-g.add_edge(0, 2)  # A - C
-g.add_edge(2, 3)  # C - D
-g.add_edge(3, 4)  # D - E
-g.add_edge(3, 5)  # D - F
-g.add_edge(3, 6)  # D - G
-g.add_edge(4, 5)  # E - F
+edges = [(1, 0), (0, 3), (0, 2), (2, 3), (3, 4), (3, 5), (3, 6), (4, 5)]
+for u, v in edges:
+    g.add_edge(u, v)
 
 print("Graph has cycle:", g.is_cyclic())
-
-#Python
