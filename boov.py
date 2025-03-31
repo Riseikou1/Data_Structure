32cd676e9425b6b8c626c7be1f33c70a59739e94
Tmk <temka4705@gmail.com>
2025-03-31 10:49:20 +0900

    Update boov.py

--- boov.py
+++ boov.py
@@ -1,78 +1,83 @@
-class Poly:
-    def __init__(self, capacity=10):
-        self.degree = 0
-        self.coef = [0] * capacity if capacity else []
+class Graph :
+    def __init__(self,size):
+        self.adj_matrix = [[0]*size for _ in range(size)]
+        self.size = size
+        self.vertex_data = ['']*size
 
-    def readPoly(self):
-        self.degree = int(input("Input polynomial degree: "))
-        for i in range(self.degree, -1, -1):
-            c = int(input("%d차 항의 계수: " %i))
-            self.coef[i] = c
+    def add_edge(self,u,v,capacity):
+        self.adj_matrix[u][v] = capacity
+
+    def add_vertex_data(self,vertex,data):
+        if 0<=vertex<self.size :
+            self.vertex_data[vertex] = data
+    
+    def dfs(self,s,t,visited=None,path=None) :
+        if visited is None :
+            visited = [False]*self.size  # visited array to avoid revisiting the same vertices during the search for an augmented path.
+        if path is None :
+            path = []    # Vertices that belong to the augmented path are stored in the path array.
+
+        visited[s] = True
+        path.append(s)
+
+        if s == t :  # if current vertex is the sink node,we have found an augmented path from the source vertex to the sink vertex,so that path can be returned.
+            return path
         
-    def printPoly(self):
-        strr = ""
-        for i in range(self.degree, 0, -1):
-            coef = self.coef[i]
-            if coef != 0:
-                sign = " + " if coef > 0 and strr else (" - " if coef < 0 and strr else ("-" if coef < 0 else ""))
-                abs_coef = abs(coef)
-                if i == 1:
-                    strr += f"{sign}{abs_coef}x"
-                else:
-                    strr += f"{sign}{abs_coef}x^{i}"
+        for ind, val in enumerate(self.adj_matrix[s]) :
+            if not visited[ind] and val > 0 :
+                result_path = self.dfs(ind,t,visited,path.copy())
+                if result_path :
+                    return result_path
+        
+        return None  # if no path is found.
+    
+    def fordFulkerson(self,source,sink) :
+        max_flow = 0
+        path = self.dfs(source,sink)
+        while path :
+            path_flow = float("inf")
+            for i in range(len(path)-1):
+                u,v = path[i] , path[i+1]
+                path_flow = min(path_flow,self.adj_matrix[u][v])
 
-        if self.coef[0] != 0:
-            sign = " + " if self.coef[0] > 0 and strr else (" - " if self.coef[0] < 0 and strr else ("-" if self.coef[0] < 0 else ""))
-            strr += f"{sign}{abs(self.coef[0])}"
+            for i in range(len(path)-1) :
+                u,v = path[i] , path[i+1]
+                self.adj_matrix[u][v] -= path_flow
+                self.adj_matrix[v][u] += path_flow
+            
+            max_flow += path_flow
 
-        print(strr)
+            path_names = [self.vertex_data[node] for node in path] 
+            print("Path:"," -> ".join(path_names), ", Flow :",path_flow)
 
+            path = self.dfs(source,sink)
 
-    def add(self):
-        second = Poly()
-        second.readPoly()
+        return max_flow
+    
 
-        max_deg = self.degree if self.degree > second.degree else second.degree
+g = Graph(6)
+vertex_names = ['s', 'v1', 'v2', 'v3', 'v4', 't']
+for i, name in enumerate(vertex_names):
+    g.add_vertex_data(i, name)
 
-        a_coef = self.coef + [0] * (max_deg - self.degree)
-        b_coef = second.coef + [0] * (max_deg - second.degree)
+g.add_edge(0, 1, 3)  # s  -> v1, cap: 3
+g.add_edge(0, 2, 7)  # s  -> v2, cap: 7
+g.add_edge(1, 3, 3)  # v1 -> v3, cap: 3
+g.add_edge(1, 4, 4)  # v1 -> v4, cap: 4
+g.add_edge(2, 1, 5)  # v2 -> v1, cap: 5
+g.add_edge(2, 4, 3)  # v2 -> v4, cap: 3
+g.add_edge(3, 4, 3)  # v3 -> v4, cap: 3
+g.add_edge(3, 5, 2)  # v3 -> t,  cap: 2
+g.add_edge(4, 5, 6)  # v4 -> t,  cap: 6
 
-        result_coef = [a_coef[i] + b_coef[i] for i in range(max_deg + 1)]
+source = 0; sink = 5
 
-        print("Sum of Polynomials:")
+print("The maximum possible flow is %d " % g.fordFulkerson(source, sink))
+    
 
-        strr = ""
-        for i in range(len(result_coef)-1, 0, -1):
-            coef = result_coef[i]
-            if coef != 0:
-                sign = " + " if coef > 0 and strr else (" - " if coef < 0 and strr else ("-" if coef < 0 else ""))
-                abs_coef = abs(coef)
-                if i == 1:
-                    strr += f"{sign}{abs_coef}x"
-                else:
-                    strr += f"{sign}{abs_coef}x^{i}"
+#class with size
+#add_edge
+# add_vertex_data
+# dfs => source,sink,visited,path
+# fordFUlkerson => source,sink
 
-        if result_coef[0] != 0:
-            sign = " + " if result_coef[0] > 0 and strr else (" - " if result_coef[0] < 0 and strr else ("-" if result_coef[0] < 0 else ""))
-            strr += f"{sign}{abs(result_coef[0])}"
-
-        print(strr)
-
-    def eval(self):
-        b = 0
-        value = int(input("Please enter x value:\n"))
-        for i in range(self.degree,0,-1):
-            b += (value**i)*self.coef[i]
-        return b + self.coef[0]
-
-
-if __name__ == "__main__":
-    a = Poly()
-    a.readPoly()
-    a.printPoly()
-    result = a.eval()
-    print(f"Result of evaluation: {result}\n")
-
-    print("Adding to 2 polynomials\n")
-
-    a.add()
