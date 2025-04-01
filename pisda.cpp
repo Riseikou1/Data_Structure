564b28b8fa7142c4ce135cb62a387b7466fd97ba
Tmk <temka4705@gmail.com>
2025-04-01 16:35:11 +0900

    Update prims to pisda.cpp

--- pisda.cpp
+++ pisda.cpp
@@ -1,91 +1,92 @@
-class Node :
-    def __init__(self,data):
-        self.data = data
-        self.left = None
-        self.right = None
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
 
-class BinaryTree :
-    def __init__(self):
-        self.root = None   
-    
-    def insert(self,data):
-        self.root = self.insertHelper(self.root,data)
-    
-    def insertHelper(self,root,data):
-        if root is None :
-            return Node(data)
-        if data < root.data :
-            root.left = self.insertHelper(root.left,data)
-        else:
-            root.right = self.insertHelper(root.right,data)
+class Graph {
+    int size;
+    vector<vector<int>> adj_matrix;
+    vector<string> vertex_data;
 
-        return root
+public:
+    Graph(int size) : size(size), adj_matrix(size, vector<int>(size, 0)), vertex_data(size, "") {}
 
-    def display(self):
-        self.displayHelper(self.root)
-        print()
+    void add_vertex_data(int vertex, const string& data) {
+        if (vertex >= 0 && vertex < size) {
+            vertex_data[vertex] = data;
+        }
+    }
 
-    def displayHelper(self,root):
-        if root :
-            self.displayHelper(root.left)
-            print(root.data,end=" ")
-            self.displayHelper(root.right)
-    def search(self,data):
-        return self.searchHelper(self.root,data)
+    void add_edge(int u, int v, int weight) {
+        if (u >= 0 && u < size && v >= 0 && v < size) {
+            adj_matrix[u][v] = weight;
+            adj_matrix[v][u] = weight; // Undirected
+        }
+    }
 
-    def searchHelper(self,root,data):
-        if root is None :
-            return False
-        if root.data == data :
-            return True
-        if data < root.data:
-            return self.searchHelper(root.left, data)
-        else:
-            return self.searchHelper(root.right, data)
-    
-    def remove(self,data):
-        if self.search(data):
-            self.root = self.removeHelper(self.root,data)
-        else :
-            print("Couldn't find the data.")
+    void prims_algorithm() {
+        vector<int> key(size, INT_MAX);
+        vector<int> parent(size, -1);
+        vector<bool> in_mst(size, false);
 
-    def removeHelper(self,root,data):
-        if root is None :
-            return root
-        if data < root.data :
-            root.left = self.removeHelper(root.left,data)
-        elif data > root.data :
-            root.right = self.removeHelper(root.right,data)
-        else :
-            if root.left is None and root.right is None :
-                return None
-            elif root.right is not None :
-                root.data = self.successor(root)
-                root.right = self.removeHelper(root.right,root.data)
-            else :
-                root.data = self.predecessor(root)
-                root.left = self.removeHelper(root.left,root.data)
-        return root
-    
-    def successor(self,root) :
-        root = root.right
-        while(root.left is not None) :
-            root = root.left
-        return root.data
-    
-    def predecessor(self,root):
-        root = root.left
-        while root.right is not None :
-            root = root.right
-        return root.data
-    
-if __name__ == "__main__" :
-    tree = BinaryTree()
-    for value in [5,1,9,2,7,3,6,4,8] :
-        tree.insert(value)
-    
-    tree.remove(4)
-    tree.display()
-    print(tree.search(4))
+        key[0] = 0;
 
-    
+        for (int count = 0; count < size; ++count) {
+            // Find the minimum key vertex not yet included in MST
+            int u = -1;
+            int min_key = INT_MAX;
+            for (int v = 0; v < size; ++v) {
+                if (!in_mst[v] && key[v] < min_key) {
+                    min_key = key[v];
+                    u = v;
+                }
+            }
+
+            if (u == -1) break; // disconnected graph
+            in_mst[u] = true;
+
+            // Update key and parent of adjacent vertices
+            for (int v = 0; v < size; ++v) {
+                int weight = adj_matrix[u][v];
+                if (weight && !in_mst[v] && weight < key[v]) {
+                    key[v] = weight;
+                    parent[v] = u;
+                }
+            }
+        }
+
+        cout << string(40, '*') << "\nPrim's Algorithm - Minimum Spanning Tree:\n" << string(40, '*') << endl;
+        cout << "Edge\tWeight" << endl;
+        int total_weight = 0;
+        for (int v = 1; v < size; ++v) {
+            int u = parent[v];
+            int weight = adj_matrix[u][v];
+            cout << vertex_data[u] << " - " << vertex_data[v] << "\t" << weight << endl;
+            total_weight += weight;
+        }
+        cout << "Total Weight of MST : " << total_weight << endl;
+    }
+};
+
+int main() {
+    Graph g(8);
+    vector<string> labels = {"A", "B", "C", "D", "E", "F", "G", "H"};
+
+    for (int i = 0; i < labels.size(); ++i) {
+        g.add_vertex_data(i, labels[i]);
+    }
+
+    vector<tuple<int, int, int>> edges = {
+        {0, 1, 4}, {0, 3, 3}, {1, 2, 3}, {1, 3, 5}, {1, 4, 6},
+        {2, 4, 4}, {2, 7, 2}, {3, 4, 7}, {3, 5, 4},
+        {4, 5, 5}, {4, 6, 3}, {5, 6, 7}, {6, 7, 5}
+    };
+
+    for (const auto& [u, v, w] : edges) {
+        g.add_edge(u, v, w);
+    }
+
+    g.prims_algorithm();
+
+    return 0;
+}
