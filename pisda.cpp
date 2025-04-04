b8cb60fb369afc427001762bb17580bab2afe390
Tmk <temka4705@gmail.com>
2025-04-04 21:10:06 +0900

    Update pisda.cpp

--- pisda.cpp
+++ pisda.cpp
@@ -1,62 +1,64 @@
 #include <iostream>
-#include <iomanip>
+#include <fstream>
 #include <ctime>    
 using namespace std;
 
 int getDaysInMonth(int month, int year) {
-
     const int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
     if (month == 2 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
-        return 29;
+        return 29; 
     }
-    return daysInMonth[month - 1]; 
+    return daysInMonth[month - 1];
 }
-void printCalendar(int month, int year) {
 
+void printCalendar(int month, int year) {
     struct tm firstDay = { 0 };
     firstDay.tm_year = year - 1900;  
-    firstDay.tm_mon = month - 1;    
-    firstDay.tm_mday = 1;       
+    firstDay.tm_mon = month - 1;     
+    firstDay.tm_mday = 1;            
     mktime(&firstDay);              
 
     int startingDay = firstDay.tm_wday; 
+    int daysInMonth = getDaysInMonth(month, year); 
 
     cout << year << " " << month << "\n";
 
     for (int i = 0; i < startingDay; i++) {
-        cout << "0 "; 
+        cout << "0 ";
     }
 
-    int daysInMonth = getDaysInMonth(month, year);
-
     for (int day = 1; day <= daysInMonth; day++) {
-        cout << day << " ";  
-        if ((startingDay + day) % 7 == 0) {
-            cout << endl;
+        cout << day;
+        if ((startingDay + day) % 7 != 0) { 
+            cout << " "; 
+        } else {
+            cout << endl; 
         }
     }
 
     int remainingDays = (startingDay + daysInMonth) % 7;
     if (remainingDays != 0) {
-        for (int i = remainingDays; i < 7; i++) {
+        for (int i = remainingDays; i < 6; i++) { 
             cout << "0 ";  
         }
+        cout << "0";  
     }
-    cout << endl;  
+    cout << endl;
 }
+
 int main() {
-    int numiter;
-    cin >> numiter;
+    int itercount;
+    ifstream infile("inputt.txt");
 
-    for(int i=0;i<numiter;i++){
+    infile >> itercount;
 
+    for (int i = 0; i < itercount; i++) {
         int year, month;
+        infile >> year >> month; 
 
-        cin >> year;
-        cin >> month;
-    
-        printCalendar(month, year);
+        printCalendar(month, year); 
     }
+
     return 0;
 }
